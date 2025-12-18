f7cdf30b2c2cb300e696a8203500b9f945ac940f
Jorge-Sanchez-dev <jorgesl099@gmail.com>
2025-12-18 17:06:23 +0100

    examen

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,3 @@
 export const COLLECTION_TRAINERS = "trainers";
 export const COLLECTION_POKEMONS = "pokemons";
 export const COLLECTION_OWNED_POKEMONS = "owned_pokemons";
-
-
-export const DEFAULT_PAGE_SIZE = 10;
-export const MAX_POKEMONS_PER_TRAINER = 6;
